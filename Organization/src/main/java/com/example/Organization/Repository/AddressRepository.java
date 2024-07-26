36e8c9de642ff348bb934406226056b29d21d4e5
sankargit9 <sivasankarnakka162@gmail.com>
2024-07-26 20:35:23 +0530

    added

--- Organization/src/main/java/com/example/Organization/Repository/AddressRepository.java
+++ Organization/src/main/java/com/example/Organization/Repository/AddressRepository.java
@@ -2,7 +2,9 @@ package com.example.Organization.Repository;
 
 import com.example.Organization.Entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Integer> {
     Address findByCity(String city);
 }
